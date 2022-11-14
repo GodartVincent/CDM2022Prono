028943dc782d8ad2cc1dbe84ea863d80614741df
Adrien Gregorj <adrien.gregorj@gmail.com>
2022-11-14 21:00:16 +0900

    formating

--- prono/polls/views.py
+++ prono/polls/views.py
@@ -4,24 +4,40 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import Exists, OuterRef
 
+
 def indexPolls(request):
     polls = Poll.objects.all()
-    context = {'polls': polls}
-    return render(request, 'polls/index.html', context)
+    context = {"polls": polls}
+    return render(request, "polls/index.html", context)
 
 
 def detailPoll(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
-    subquery = QuestionChoice.objects.filter(question=OuterRef('pk'), user=request.user)
-    questions = poll.question_set.all().annotate(isfilled=Exists(subquery), prono=subquery.values("choice"), points=subquery.values("points"))
-    subquery = MatchChoice.objects.filter(match=OuterRef('pk'), user=request.user)
-    matchs = poll.match_set.all().annotate(isfilled=Exists(MatchChoice.objects.filter(match=OuterRef('pk'), user=request.user)), prono_1=subquery.values("score_1"), prono_2=subquery.values("score_2"), points=subquery.values("points"))
-    return render(request, 'polls/detail.html', {'poll': poll, 'matchs' : matchs, 'questions' : questions})
+    subquery = QuestionChoice.objects.filter(question=OuterRef("pk"), user=request.user)
+    questions = poll.question_set.all().annotate(
+        isfilled=Exists(subquery),
+        prono=subquery.values("choice"),
+        points=subquery.values("points"),
+    )
+    subquery = MatchChoice.objects.filter(match=OuterRef("pk"), user=request.user)
+    matchs = poll.match_set.all().annotate(
+        isfilled=Exists(
+            MatchChoice.objects.filter(match=OuterRef("pk"), user=request.user)
+        ),
+        prono_1=subquery.values("score_1"),
+        prono_2=subquery.values("score_2"),
+        points=subquery.values("points"),
+    )
+    return render(
+        request,
+        "polls/detail.html",
+        {"poll": poll, "matchs": matchs, "questions": questions},
+    )
 
 
 def detailMatch(request, match_id):
     match = get_object_or_404(Match, pk=match_id)
-    return render(request, 'matchs/detail.html', {'match': match})
+    return render(request, "matchs/detail.html", {"match": match})
 
 
 def pronosticMatch(request, match_id):
@@ -29,10 +45,14 @@ def pronosticMatch(request, match_id):
         match = get_object_or_404(Match, pk=match_id)
         if match.isPronoOver():
             # Redisplay the match pronosticing form.
-            return render(request, 'polls/detail.html', {
-                'match': match,
-                'error_message': "Il est trop tard pour pronostiquer sur ce match.",
-            })
+            return render(
+                request,
+                "polls/detail.html",
+                {
+                    "match": match,
+                    "error_message": "Il est trop tard pour pronostiquer sur ce match.",
+                },
+            )
         try:
             matchChoice = match.matchchoice_set.get(user_id=request.user)
         except (KeyError, MatchChoice.DoesNotExist):
@@ -44,16 +64,22 @@ def pronosticMatch(request, match_id):
             matchChoice.score_2 = int(scores[1])
         except (ValueError):
             # Redisplay the match pronosticing form.
-            return render(request, 'polls/detail.html', {
-                'match': match,
-                'error_message': "Vous n'avez pas rempli les 2 scores.",
-            })
+            return render(
+                request,
+                "polls/detail.html",
+                {
+                    "match": match,
+                    "error_message": "Vous n'avez pas rempli les 2 scores.",
+                },
+            )
         else:
             matchChoice.save()
             # Always return an HttpResponseRedirect after successfully dealing
             # with POST data. This prevents data from being posted twice if a
             # user hits the Back button.
-            return HttpResponseRedirect(reverse('polls:detailPoll', args=(match.poll.id,)))
+            return HttpResponseRedirect(
+                reverse("polls:detailPoll", args=(match.poll.id,))
+            )
 
 
 def pronosticQuestion(request, question_id):
@@ -61,10 +87,14 @@ def pronosticQuestion(request, question_id):
         question = get_object_or_404(Question, pk=question_id)
         if question.isPronoOver():
             # Redisplay the match pronosticing form.
-            return render(request, 'polls/detail.html', {
-                'question': question,
-                'error_message': "Il est trop tard pour pronostiquer sur cette question.",
-            })
+            return render(
+                request,
+                "polls/detail.html",
+                {
+                    "question": question,
+                    "error_message": "Il est trop tard pour pronostiquer sur cette question.",
+                },
+            )
         try:
             questionChoice = question.questionchoice_set.get(user_id=request.user)
         except (KeyError, QuestionChoice.DoesNotExist):
@@ -74,18 +104,24 @@ def pronosticQuestion(request, question_id):
             questionChoice.choice = request.POST["answer"]
         except (ValueError):
             # Redisplay the question pronosticing form.
-            return render(request, 'polls/detail.html', {
-                'question': question,
-                'error_message': "Vous n'avez pas repondu à la question.",
-            })
+            return render(
+                request,
+                "polls/detail.html",
+                {
+                    "question": question,
+                    "error_message": "Vous n'avez pas repondu à la question.",
+                },
+            )
         else:
             questionChoice.save()
             # Always return an HttpResponseRedirect after successfully dealing
             # with POST data. This prevents data from being posted twice if a
             # user hits the Back button.
-            return HttpResponseRedirect(reverse('polls:detailPoll', args=(question.poll.id,)))
+            return HttpResponseRedirect(
+                reverse("polls:detailPoll", args=(question.poll.id,))
+            )
+
 
 # def results(_, match_id):
 #     response = "Vous regardez les résultats du match %s."
 #     return HttpResponse(response % match_id)
-
